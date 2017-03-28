e8e4ae239dd74928e59bc3fc1c77cd2fcb03f14a
James B. Crocker <james.crocker@us.sios.com>
2017-03-28 10:22:35 -0400

    Updates to Selenium 3.3.1, ChromeDrvier 2.28 and GeckoDriver 0.15

--- windows/start-selenium.bat
+++ windows/start-selenium.bat
@@ -6,7 +6,7 @@ REM Batch to start selenium
 
 
 
-Start java -jar c:\selenium\selenium-server-standalone-3.0.1.jar -log c:\selenium\selenium-node.log -role node -nodeConfig c:\selenium\nodeConfig-WIN10.json
+Start java -jar c:\selenium\selenium-server-standalone.jar -log c:\selenium\selenium-node.log -role node -nodeConfig c:\selenium\nodeConfig-WIN10.json
 
 
 
